541b7cc08518c6285a08555d0af940b882abf35e
Noah Eakin <noaheakin@gmail.com>
2020-08-10 19:29:16 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,11 +1,1 @@
 def count_strings(array)
-  array.count do |element|
-    element.instance_of? String
-  end
-end
-
-def count_empty_strings(array)
-  array.count do |element|
-    element == ""
-  end
-end
